675a13ffc78ed8afa96ceb34578a4b568609d6dd
KuzyaCat <lizerd34@mail.ru>
2020-10-05 14:58:27 +0300

    Add single target healer logic

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -4,6 +4,7 @@ import { Creep } from './units/Creep';
 import { RangeCreep } from './units/RangeCreep';
 import { Mage } from './units/Mage';
 import { Druid } from './units/Druid';
+import { HealerCreep } from './units/HealerCreep';
 import { boardLocation, unit } from './types';
 
 export class Game {
@@ -230,6 +231,57 @@ export class Game {
         healer as Unit,
         healer.getPossibleTargets(location.getUnitBoardLocation(healer) as boardLocation, location),
       );
+    }
+    console.log(board.getBoardMatrix());
+
+    return JSON.stringify(board.getBoardMatrix());
+  }
+
+  static case8(rowsCount: number, columnsCount: number): string {
+    const board = new Board(rowsCount, columnsCount);
+    const location: Location = new Location(board);
+    const action: Action = new Action(location, board);
+
+    const units: unit[][] = Array<Array<Unit>>(rowsCount);
+    for (let i = 0; i < rowsCount; i += 1) {
+      const unitsRow = Array<Unit>(columnsCount);
+      if (i === 0) {
+        for (let j = 0; j < columnsCount; j += 1) {
+          unitsRow[j] = new RangeCreep('kek', 100, 20, 20);
+          units[i] = unitsRow;
+        }
+        continue;
+      }
+      for (let j = 0; j < columnsCount; j += 1) {
+        unitsRow[j] = new Creep('kek', 100, 20, 20);
+      }
+      units[i] = unitsRow;
+    }
+    units[0][2] = new HealerCreep('kek', 100, 10, 10);
+    units[3][2] = new Mage('kek', 100, 40, 40);
+    units[2][2] = null;
+    board.fillWithUnits(units);
+
+    const allyLocation = {
+      rowIndex: 3,
+      columnIndex: 1,
+    };
+
+    const attacker = units[3][2];
+    const rangeAttacker = units[0][1];
+    const healer = units[0][2];
+    console.log(board.getBoardMatrix());
+    if (attacker) {
+      action.deal(
+        attacker as Unit,
+        attacker.getPossibleTargets(location.getUnitBoardLocation(attacker) as boardLocation, location),
+      );
+    }
+    if (rangeAttacker) {
+      action.deal(rangeAttacker as Unit, [allyLocation]);
+    }
+    if (healer) {
+      action.deal(healer as Unit, [allyLocation]);
     }
     console.log(board.getBoardMatrix());
 
